d3e146545267a26f5a6fdb4b4c51fced17e7cb7d
gareth <gareth@PaloAlto.(none)>
2010-11-10 15:39:44 -0500

    includes flow from select screen to info screen with wireframe ui

--- src/android/pizzabutton/orderpizza.java
+++ src/android/pizzabutton/orderpizza.java
@@ -2,12 +2,38 @@ package android.pizzabutton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class orderpizza extends Activity {
+	  public void ToastMsg(String message) {
+		    Toast msg = Toast.makeText(
+		        getApplicationContext(), 
+		        message,
+		        Toast.LENGTH_LONG);
+
+		    msg.show();
+	}
+	  
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button button = (Button)findViewById(R.id.pizza);
+        button.setOnClickListener((android.view.View.OnClickListener) pizzaButtonListener);
+    }
+    
+    private OnClickListener pizzaButtonListener = new OnClickListener() {
+    	public void onClick(View v) {
+    		setContentView(R.layout.info);
+    	}
+    };
+    
+    private void loadPageInfo() {
+    	
     }
 }
